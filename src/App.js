5fd338b2faad34849b59fc2f3ee4e43b21442150
Vladimir <opaozhub@gmail.com>
2019-09-13 23:44:59 +0300

    feat(language): implemented language picker
    Available languages: US, JP, CN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import ReactFlagsSelect from 'react-flags-select';
 
-import './App.css';
 import PokemonCard from './components/pokemon-card/PokemonCard';
 import pokemonListRaw from './resources/pokemon/pokedex.json';
+
+import './App.css';
+import 'react-flags-select/css/react-flags-select.css';
 
 const images = require.context('./resources/pokemon/thumbnails', true);
 
@@ -10,6 +13,7 @@ function App() {
 	const [isReady, setReady] = useState(false);
 	const [pokemonList, setPokemonList] = useState([]);
 	const [selectedSet, setSelectedSet] = useState(new Set());
+	const [language, setLanguage] = useState('US');
 
 	useEffect(() => {
 		if (isReady) {
@@ -19,7 +23,11 @@ function App() {
 
 		pokemonListRaw.forEach(pokemon => {
 			const { id: rawId, name = {} } = pokemon;
-			const { english: engName = 'unknown' } = name;
+			const {
+				english = 'unknown',
+				japanese = 'unknown',
+				chinese = 'unknown',
+			} = name;
 
 			let id = `${rawId}`;
 
@@ -27,7 +35,14 @@ function App() {
 				id = `000${id}`.substr(-3);
 			}
 
-			list.push({ id, name: engName });
+			list.push({
+				id,
+				name: {
+					US: english,
+					JP: japanese,
+					CN: chinese,
+				},
+			});
 		});
 
 		setPokemonList(list);
@@ -43,8 +58,19 @@ function App() {
 		}
 	};
 
+	const handleSelect = lg => {
+		setLanguage(lg);
+	};
+
 	return (
 		<div className="App">
+			<div className="App-langpicker">
+				<ReactFlagsSelect
+					onSelect={handleSelect}
+					defaultCountry="US"
+					countries={['US', 'JP', 'CN']}
+				/>
+			</div>
 			<header className="App-header">Create you favourite Pokemons!</header>
 			<main className="App-body">
 				<div className="App-controls"></div>
@@ -54,7 +80,7 @@ function App() {
 							id={id}
 							key={id}
 							isFirst={key === 0}
-							name={name}
+							name={name[language]}
 							images={images}
 							isSelected={selectedSet.has(id)}
 							onChange={checked => handlePokemonCheck(id, checked)}
